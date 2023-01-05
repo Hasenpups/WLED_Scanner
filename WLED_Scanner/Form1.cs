f40f0c8258a1911aa6e97236152198e328ba2626
Hasenpups <stefan.riese@me.com>
2023-01-05 08:49:12 +0100

    initial commit

---
+++ WLED_Scanner/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WLED_Controller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static UdpClient udpListener = new UdpClient();
+        static int localPort = 65506;
+        static IPEndPoint localEP = new IPEndPoint(IPAddress.Any, localPort);
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            UdpClient c = (UdpClient)ar.AsyncState;
+            IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+            c.BeginReceive(DataReceived, ar.AsyncState);
+
+            // parse UDP data
+            if (receivedBytes.Length > 0)
+            {
+                String ipAddress = receivedBytes[2] + "." + receivedBytes[3] + "." + receivedBytes[4] + "." + receivedBytes[5];
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        // check if we already have this device in the list
+                    var item = listView1.FindItemWithText(ipAddress);
+
+                    // check if device was found
+                    if (item == null)
+                    {
+                        // get info about WLED device
+                        var wledInfo = GetInfo(ipAddress);
+                        WledInstance instance = JsonConvert.DeserializeObject<WledInstance>(wledInfo);
+
+                        TimeSpan t = TimeSpan.FromSeconds(instance.uptime);
+
+                        // create single row
+                        string[] row = { instance.name, instance.ip, instance.ver, instance.arch, t.ToString(@"dd\:hh\:mm\:ss")};
+
+                        // create list view item
+                        ListViewItem newListItem = new ListViewItem(row);
+
+                        // add item to listview
+                        listView1.Items.Add(newListItem);
+                    }
+                });
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            udpListener.ExclusiveAddressUse = false;
+            udpListener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpListener.Client.Bind(localEP);
+            udpListener.BeginReceive(DataReceived, udpListener);
+        }
+
+        private string GetInfo(string ip)
+        {
+            string checkResult = null;
+            var httpClient = new HttpClient();
+
+            try
+            {
+                Task<string> t = httpClient.GetStringAsync("http://" + ip + "/json/info");
+                checkResult = t.Result;
+            }
+            catch
+            {
+            }
+
+            httpClient.Dispose();
+
+            return checkResult;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // check if listview item is selected
+            if(listView1.SelectedItems.Count > 0)
+            {
+                OpenBrowser("http://" + listView1.SelectedItems[0].SubItems[1].Text);
+            }
+        }
+
+        private void OpenBrowser(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+}
---
+++ WLED_Scanner/WledInstance.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLED_Controller
+{
+        public class Fs
+        {
+            public int u { get; set; }
+            public int t { get; set; }
+            public int pmt { get; set; }
+        }
+
+        public class Leds
+        {
+            public int count { get; set; }
+            public int pwr { get; set; }
+            public int fps { get; set; }
+            public int maxpwr { get; set; }
+            public int maxseg { get; set; }
+            public List<int> seglc { get; set; }
+            public int lc { get; set; }
+            public bool rgbw { get; set; }
+            public int wv { get; set; }
+            public int cct { get; set; }
+        }
+
+        public class WledInstance
+    {
+            public string ver { get; set; }
+            public int vid { get; set; }
+            public Leds leds { get; set; }
+            public bool str { get; set; }
+            public string name { get; set; }
+            public int udpport { get; set; }
+            public bool live { get; set; }
+            public int liveseg { get; set; }
+            public string lm { get; set; }
+            public string lip { get; set; }
+            public int ws { get; set; }
+            public int fxcount { get; set; }
+            public int palcount { get; set; }
+            public int cpalcount { get; set; }
+            public List<int> maps { get; set; }
+            public Wifi wifi { get; set; }
+            public Fs fs { get; set; }
+            public int ndc { get; set; }
+            public string arch { get; set; }
+            public string core { get; set; }
+            public int lwip { get; set; }
+            public int freeheap { get; set; }
+            public int uptime { get; set; }
+            public int opt { get; set; }
+            public string brand { get; set; }
+            public string product { get; set; }
+            public string mac { get; set; }
+            public string ip { get; set; }
+        }
+
+        public class Wifi
+        {
+            public string bssid { get; set; }
+            public int rssi { get; set; }
+            public int signal { get; set; }
+            public int channel { get; set; }
+        }
+}
